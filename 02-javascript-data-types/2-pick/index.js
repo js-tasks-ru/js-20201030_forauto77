b274635fdf6155f15ba4b38b22ca86dcdd9e1ffb
Roman <forauto77@yandex.ru>
2020-11-05 19:17:02 +0300

    solved 1-module task-2 and task-3

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,12 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+    const newObj = {};
+    for (let key in obj) {
+        if (fields.includes(key)) {
+           newObj[key] = obj[key];
+        }
+    };
 
+    return newObj
 };
