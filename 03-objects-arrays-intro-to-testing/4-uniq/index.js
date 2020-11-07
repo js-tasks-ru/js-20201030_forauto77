82944828763b9b7f47e1e051107f455299d64f94
Roman <forauto77@yandex.ru>
2020-11-07 21:01:25 +0300

    solved 3-module

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,11 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+    const set = new Set();
+    if (arr) {
+        arr.forEach((item) => set.add(item))
+    };
+    return Array.from(set);
 }
+
+
