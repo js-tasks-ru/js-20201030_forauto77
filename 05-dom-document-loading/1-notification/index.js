4ae7b43b9b05b40215c7d7451dd42752c9c2e6d0
Roman <forauto77@yandex.ru>
2020-11-17 18:24:30 +0300

    solved 5-module 1-task

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,51 @@
 export default class NotificationMessage {
+    element;
+    static showedElement;
 
+    constructor(message = '', { duration = 1000, type = 'success' } = {}) {
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+
+        if (NotificationMessage.showedElement) {
+            NotificationMessage.showedElement.remove();
+        }
+
+        this.render();
+    }
+
+    render() {
+        
+        this.element = document.createElement('div');
+        this.element.innerHTML = `
+        <div class="notification success" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">success</div>
+          <div class="notification-body">
+            ${this.message + Math.random()}
+          </div>
+        </div>
+        </div>
+        `;
+        this.element = this.element.firstElementChild;
+        NotificationMessage.showedElement = this.element;
+    }
+
+    show() {      
+        document.body.append(this.element);
+        setTimeout(() => (this.remove()), this.duration);   
+        
+        return this.element
+        
+    }
+
+    remove() {
+        this.element.remove();
+    }
+  
+    destroy() {
+        this.remove();
+        this.element = null;
+    }
 }
